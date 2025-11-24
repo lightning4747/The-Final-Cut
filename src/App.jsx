da009ed2c88dce85dbd7d240579fdf3e5b4f334b
lightning4747 <vignesh112847@gmail.com>
2025-11-24 21:35:35 +0530

    implemented moviecard and loading screen animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import { useEffect,useState } from 'react'
-import Search from './components/search'
-import LoadingSpinner from './components/LoadingSpinner';
+import { useEffect, useState } from 'react'
+import Search from './components/Search'
+import LoadingSpinner from './components/LoadingSpinner'
+import { MovieCard } from './components/MovieCard'
 
-const API_BASE_URL = "https://api.themoviedb.org/3";
-
-const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+const API_BASE_URL = "https://api.themoviedb.org/3"
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY
 
 const API_OPTIONS = {
   method: 'GET',
@@ -15,69 +15,62 @@ const API_OPTIONS = {
 }
 
 const App = () => {
-  const [searchTerm, setsearchTerm] = useState('');
-  const [errormessage, seterrormessage] = useState('');
-  const [movielist, setmovielist] = useState([]);
-  const [isloading,setisloading] = useState(true);
+  const [searchTerm, setsearchTerm] = useState('')
+  const [errormessage, seterrormessage] = useState('')
+  const [movielist, setmovielist] = useState([])
+  const [isloading, setisloading] = useState(false)
 
-  const fetchMovies = async() => {
-    setisloading(true);
-    seterrormessage('');
+  const fetchMovies = async () => {
+    setisloading(true)
+    seterrormessage('')
 
     try {
-      const endpoints = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
-      const response = await fetch(endpoints, API_OPTIONS);
-      if(!response.ok) {
-        throw new Error('Failed to fetch movies');
-      }
-      const data = await response.json();
-      if(data.response ==='False') {
-        seterrormessage(data.Error  || 'Failed to fetch movies');
-        setmovielist([]);
-        return;
-      }
-      setmovielist(data.results ||[]);
-    }
-    
-    catch(error) {
-      console.error(error);
-      seterrormessage("Error fetching movies");
-    }
+      const endpoint = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`
+      const response = await fetch(endpoint, API_OPTIONS)
 
-    finally {
-      setisloading(true);
+      if (!response.ok) throw new Error('Failed to fetch movies')
+
+      const data = await response.json()
+      setmovielist(data.results || [])
+    } catch (error) {
+      console.error(error)
+      seterrormessage("Error fetching movies")
+    } finally {
+      setisloading(false)
     }
   }
-  useEffect(()=> {
-    fetchMovies();
-  }, []);
+
+  useEffect(() => {
+    fetchMovies()
+  }, [])
 
   return (
     <main>
-      <div className='pattern'></div>
-      <div className='wrapper'>
+      <div className="pattern"></div>
+      <div className="wrapper">
         <header>
-          <img src="./hero.png" alt="hero-banner" />
-          <h1>Find <span className='text-gradient'>movies</span> you'll enjoy without the hassle</h1>
-        <Search searchTerm= {searchTerm} setsearchTerm={setsearchTerm}/>
+          <img src="/hero.png" alt="hero-banner" />
+          <h1>
+            Find <span className="text-gradient">movies</span> you'll enjoy without the hassle
+          </h1>
+          <Search searchTerm={searchTerm} setsearchTerm={setsearchTerm} />
         </header>
 
-        <section className='all-movies'>
-        <h2>All movies</h2>
+        <section className="all-movies">
+          <h2 className="mt-[40px]">All movies</h2>
 
-       {isloading ? (
-        <LoadingSpinner/>
-       ): errormessage ? (
-        <p className='text-white'>{errormessage}</p>
-       ) : (
-        <ul>
-          {movielist.map((movie) => (
-            <p key={movie.id} className='text-white'>{movie.title}</p>
-          ))}
-        </ul>
-       )}
-        </section> 
-       
+          {isloading ? (
+            <LoadingSpinner />
+          ) : errormessage ? (
+            <p className="text-white">{errormessage}</p>
+          ) : (
+            <ul>
+              {movielist.map((movie) => (
+                <MovieCard key={movie.id} movie={movie} />
+              ))}
+            </ul>
+          )}
+        </section>
       </div>
     </main>
   )
